9e3de3146e7b28afe71ad8fb5b1b19233e42992e
arjunvsojan <137357590+arjunvsojan@users.noreply.github.com>
2023-07-02 21:46:32 -0400

    Create lab2.py

---
+++ lab2.py
@@ -0,0 +1,8 @@
+import sys
+def print_arguments():
+    name = sys.argv[0]
+    args = ' '.join(sys.argv[1:])
+    print("Script name:", name)
+    print("Script name and arguments:", script_name, arguments)
+#function call
+print_arguments()
