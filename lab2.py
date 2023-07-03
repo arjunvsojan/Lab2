4ce618e01887aebf7214b1ba61c7b4e6b4009660
arjunvsojan <137357590+arjunvsojan@users.noreply.github.com>
2023-07-02 22:46:01 -0400

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -4,5 +4,9 @@ def print_arguments():
     args = ' '.join(sys.argv[1:])
     print("Script name:", name)
     print("Script name and arguments:", script_name, arguments)
+def helloWorld():
+	print(‘Hello World’)
+
 #function call
 print_arguments()
+helloWorld()
